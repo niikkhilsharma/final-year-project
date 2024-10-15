5152cac64d6d9ff4e2a4bb81876b4afb0fa0a9d2
Deepali <deepalithakurjnv@gmail.com>
2024-10-15 07:34:39 +0530

    homepage

--- components/hero-page-banner.tsx
+++ components/hero-page-banner.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const HeroPageBanner = () => {
   return (
     <div className="flex h-11 w-full items-center justify-center border-b border-[#e6e2e0] bg-[#f8f4f2f7] text-sm">
-      <p className="font-medium">💵 NEW: 2024 UX Salary Report!</p>
-      <span className="ml-1.5 text-sm font-bold underline">See the data→</span>
+      <p className="font-medium">💵 NEW: Notifications!</p>
+      <span className="ml-1.5 text-sm font-bold underline">→</span>
     </div>
   );
 };
